baed3b92e67d4a338325d603e359d7bbbe72d9bf
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-05-13 18:42:47 -0300

    Criando o modelo de clientes

---
+++ src/br/com/jopaulo/vendas/model/Clientes.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jopaulo.vendas.model;
+
+/**
+ *
+ * @author joao
+ */
+public class Clientes {
+    
+    //atributos
+    private int id;
+    private String nome;
+    private String email;
+    private String celular;
+    private String telefone;
+    private String cep;
+    private String endereco;
+    private int numero;
+    private String uf;
+    private String cidade;
+    private String bairro;
+    private String complemento;
+    private String cpf;
+    private String rg;
+    
+}
